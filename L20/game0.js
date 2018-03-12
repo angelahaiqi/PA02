d05801085fae8f11135c239bd9805570a7f109be
luyidai <35776619+luyidai@users.noreply.github.com>
2018-03-12 18:54:12 -0400

    Update game0.js

--- L20/game0.js
+++ L20/game0.js
@@ -102,9 +102,11 @@ The user moves a cube around the board trying to knock balls into a cone
 			cone.position.set(10,3,7);
 			scene.add(cone);
 
-			torusKnot = createNPC();
-			torusKnot.position.set(40, 8, 30);
-			scene.add(torusKnot);
+			npc = createTorusKnot(0xff0000);
+			npc.position.set(40, 8, 30);
+			scene.add(npc);
+		
+		
 			//playGameMusic();
 
 	}
@@ -319,9 +321,9 @@ The user moves a cube around the board trying to knock balls into a cone
 		return mesh;
 	}
 
-	function createNPC() {
+	function createTorusKnot(color) {
 		var geometry = new THREE.TorusKnotGeometry(5, 3, 10, 15);
-		var material = new THREE.MeshLambertMaterial( { color: 0xff0000} );
+		var material = new THREE.MeshLambertMaterial( { color: color} );
 		var pmaterial = new Physijs.createMaterial(material,0.9,0.5);
 		var mesh = new Physijs.BoxMesh(geometry, pmaterial, 0);
 		mesh.castShadow = true;

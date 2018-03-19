b33c89dd02dd7bde38c1965d4bb72788b23dbe06
luyidai <35776619+luyidai@users.noreply.github.com>
2018-03-19 18:51:43 -0400

    Update game0.js

--- L20/game0.js
+++ L20/game0.js
@@ -482,7 +482,7 @@ The user moves a cube around the board trying to knock balls into a cone
 		//draw heads up display ..
 	  var info = document.getElementById("info");
 		info.innerHTML='<div style="font-size:24pt">Score: ' + gameState.score 
-		+ " Health: "+gameState.health'
+		+ " Health: "+gameState.health
 		+ </div>';
 
 	}
